bbd6145039aaf1645789f2cf269c89928702d2e5
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-11-17 15:29:36 +0100

    Mail - Inserito la richiesta della mail

---
+++ mail/js/script.js
@@ -0,0 +1,19 @@
+// MAIL
+
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+// Elenco delle mail
+const mailList = ['matteo@gmail.com', 'marco@gmail.com', 'gianni@gmail.com', 'piero@gmail.com', ];
+
+
+// Chiedo la mail - UserMail Request
+const userMail = prompt('Qual è la tua mail?');
+console.log(userMail);
+
+// Controllo se la mail è nella lista
+
+
+// Stampo messaggio finale
