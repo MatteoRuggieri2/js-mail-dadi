087c2c40de6930ac00c3f27e7c8b2cdbdfcc01c8
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-11-17 16:15:25 +0100

    Dadi - Aggiunta estrazione utente

---
+++ dadi/js/script.js
@@ -0,0 +1,30 @@
+// GIOCO DEI DADI
+
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+
+// Introduzione al gioco
+alert(`
+GIOCO DEI DADI
+
+L'utente tira per primo i dadi, poi tira il pc.
+Il numero più alto vince.
+
+Clicca 'OK' per continuare
+`);
+
+// Estrazione numero utente
+let userNumber = Math.floor(Math.random() * 6) + 1;
+alert(`
+Il tuo numero estratto è: ${userNumber}
+`);
+
+// Estrazione numero pc
+
+
+// Confonto i numeri
+
+
+// Output - Esito della partita
